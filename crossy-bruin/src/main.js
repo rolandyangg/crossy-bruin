c0cb80677cc73b8a919e3cb15c155d362f80372b
roland yang <rolandyang@gmail.com>
2025-05-14 13:38:31 -0700

    Camera Setup

--- crossy-bruin/src/main.js
+++ crossy-bruin/src/main.js
@@ -1,24 +1,46 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import * as THREE from "three";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+// Models
+function Player() {
+  const player = new THREE.Group();
 
-setupCounter(document.querySelector('#counter'))
+  const bodyGeometry = new THREE.BoxGeometry(15, 15, 30);
+  const bodyMaterial = new THREE.MeshPhongMaterial({ color: "black" });
+  const body = new THREE.Mesh(bodyGeometry, bodyMaterial);
+  body.position.z = 10;
+  player.add(body);
+
+  return player;
+}
+
+// Camera
+const size = 300;
+const aspectRatio = window.innerWidth / window.innerHeight;
+let width = size * aspectRatio;
+let height = size;
+if (aspectRatio < 1) {
+  width = size;
+  height = size / aspectRatio;
+}
+
+const camera = new THREE.OrthographicCamera(width / -2, width / 2, height / 2, height / -2, 100, 900);
+camera.up.set(0, 0, 1); // Make z-axis the up direction
+camera.position.set(200, -300, 350);
+camera.lookAt(0, 0, 0);
+
+// World
+const world = new THREE.Scene();
+const player = new Player();
+world.add(player);
+player.add(camera);
+
+// Render the game onto the screen
+const canvas = document.querySelector("canvas.app");
+const renderer = new THREE.WebGLRenderer({
+  alpha: true,
+  antialias: true,
+  canvas: canvas
+});
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.render(world, camera);
